4203cf3d976afa37ce51864a288c9179fc86f06d
ArvidUtas <arvid.utas@digitalengine.se>
2026-03-25 19:57:40 +0100

    init

---
+++ mqs/src/test/java/com/example/mqs/MqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
